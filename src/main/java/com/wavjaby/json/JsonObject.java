1829d09a99e2b59912104db627ff495069a2a672
WavJaby <WavJaby@gmail.com>
2024-07-18 17:35:33 +0800

    Add unicode decode support

--- src/main/java/com/wavjaby/json/JsonObject.java
+++ src/main/java/com/wavjaby/json/JsonObject.java
@@ -1,24 +1,40 @@
 package com.wavjaby.json;
 
+import java.io.InputStream;
 import java.io.Serializable;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
-public class JsonObject extends ValueGetter<JsonObject, String> implements Serializable {
-    private final HashMap<String, Object> map = new HashMap<>();
+public class JsonObject extends ValueGetter<JsonObject, String> implements Serializable, Iterable<Map.Entry<String, Object>> {
+    private final HashMap<String, Object> map;
 
     @SuppressWarnings("unused")
     public JsonObject() {
-
+        this.map = new HashMap<>();
     }
 
     public JsonObject(String input) {
-        this(new JsonObjectReader(input));
+        this(new JsonReader(input));
     }
 
-    public JsonObject(JsonObjectReader reader) {
+    public JsonObject(InputStream input) {
+        this(new JsonReader(input, StandardCharsets.UTF_8));
+    }
+
+    public JsonObject(InputStream input, Charset charset) {
+        this(new JsonReader(input, charset));
+    }
+
+    public JsonObject(JsonReader reader) {
+        this.map = new HashMap<>();
         reader.findJsonStart();
-        if (reader.thisChar() == '[') {
-            throw new JsonException("JsonObject must start with '['");
+
+        if (reader.thisChar() != '{') {
+            throw new JsonException("JsonObject must start with '{',", reader);
         }
         String key = null;
         boolean isValue = false;
@@ -26,21 +42,24 @@ public class JsonObject extends ValueGetter<JsonObject, String> implements Seria
         while ((nextChar = reader.nextChar()) != '\0') {
             if (nextChar == ',') {
                 if (isValue)
-                    throw new JsonException("missing value at: " + reader.i);
+                    throw new JsonException("Missing value", reader);
                 nextChar = reader.nextChar();
             }
             if (nextChar == '}') {
                 if (isValue)
-                    throw new JsonException("missing value at: " + reader.i);
+                    throw new JsonException("Missing value", reader);
                 return;
             }
             switch (nextChar) {
                 case '"':
                     if (isValue) {
                         map.put(key, reader.readString());
+                        key = null;
                         isValue = false;
-                    } else
+                    } else if (key == null)
                         key = reader.readString();
+                    else
+                        throw new JsonException("Missing ':' after a key", reader);
                     break;
                 case ':':
                     if (!isValue)
@@ -49,23 +68,42 @@ public class JsonObject extends ValueGetter<JsonObject, String> implements Seria
                 case '[':
                     if (isValue) {
                         map.put(key, new JsonArray(reader, false));
+                        key = null;
                         isValue = false;
                     }
                     break;
                 case '{':
                     if (isValue) {
                         map.put(key, new JsonObject(reader));
+                        key = null;
                         isValue = false;
                     }
                     break;
                 default:
                     if (isValue) {
                         map.put(key, reader.readValue());
+                        key = null;
                         isValue = false;
-                    }
+                    } else
+                        throw new JsonException("Missing ':' after a key", reader);
             }
         }
-        throw new JsonException("JsonObject must end with '}'");
+        throw new JsonException("JsonObject must end with '}',", reader);
+    }
+
+    public JsonObject(Map<?,?> map) {
+        if (map == null)
+            this.map = new HashMap<>();
+        else {
+            this.map = new HashMap<>(map.size());
+            for (final Map.Entry<?, ?> e : map.entrySet()) {
+                if(e.getKey() == null)
+                    throw new NullPointerException("Null key");
+                final Object value = e.getValue();
+                if (value != null)
+                    this.map.put(String.valueOf(e.getKey()), warpValue(value));
+            }
+        }
     }
 
     @SuppressWarnings("unused")
@@ -111,11 +149,12 @@ public class JsonObject extends ValueGetter<JsonObject, String> implements Seria
         StringBuilder builder = new StringBuilder();
         for (HashMap.Entry<String, Object> item : map.entrySet()) {
             builder.append(",");
-            builder.append('\"').append(item.getKey()).append('\"').append(':');
+            makeQuote(item.getKey(), builder);
+            builder.append(':');
             if (item.getValue() == null)
                 builder.append("null");
             else if (item.getValue() instanceof String)
-                builder.append('\"').append(item.getValue()).append('\"');
+                makeQuote((String) item.getValue(), builder);
             else
                 builder.append(item.getValue());
         }
@@ -129,42 +168,136 @@ public class JsonObject extends ValueGetter<JsonObject, String> implements Seria
 
     @SuppressWarnings("unused")
     public String toStringBeauty() {
-        return this.toString(1, null);
+        return this.toString(4, 1, null);
     }
 
-    String toString(int index, char[] lastTab) {
+    String toString(int tabSize, int index, char[] lastTab) {
         StringBuilder builder = new StringBuilder();
-        char[] tab = new char[index * 2];
-        for (int i = 0; i < index * 2; i++) {
-            tab[i] = ' ';
-        }
+        char[] tab = JsonObject.createTab(tabSize, index, lastTab);
         builder.append('{');
         if (map.size() > 0)
             builder.append('\n');
 
         int i = map.size();
         for (HashMap.Entry<String, Object> item : map.entrySet()) {
-            builder.append(tab).append('\"').append(item.getKey()).append(index < 0 ? "\":" : "\": ");
-            Object itemValue = item.getValue();
-            if (itemValue == null)
-                builder.append("null");
-            else if (itemValue instanceof JsonObject)
-                builder.append(((JsonObject) itemValue).toString(index + 1, tab));
-            else if (itemValue instanceof JsonArray)
-                builder.append(((JsonArray) itemValue).toString(index + 1, tab));
-            else if (itemValue instanceof String)
-                builder.append('\"').append(itemValue).append('\"');
-            else
-                builder.append(itemValue);
-
-            if (--i > 0)
-                builder.append(",");
-            builder.append("\n");
+            builder.append(tab);
+            makeQuote(item.getKey(), builder);
+            builder.append(": ");
+            appendValue(item.getValue(), --i > 0, tabSize, index, tab, builder);
         }
+
         if (lastTab != null && map.size() > 0)
             builder.append(lastTab);
-        builder.append("}");
+        builder.append('}');
 
         return builder.toString();
+    }
+
+    static void appendValue(Object value, boolean addComma, int tabSize, int index, char[] tab, StringBuilder builder) {
+        if (value == null)
+            builder.append("null");
+        else if (value instanceof JsonObject)
+            builder.append(((JsonObject) value).toString(tabSize, index + 1, tab));
+        else if (value instanceof JsonArray)
+            builder.append(((JsonArray) value).toString(tabSize, index + 1, tab));
+        else if (value instanceof ListedJsonObject)
+            builder.append(((ListedJsonObject) value).toString(tabSize, index + 1, tab));
+        else if (value instanceof String)
+            makeQuote((String) value, builder);
+        else if (value instanceof Collection)
+            builder.append(new JsonArray((Collection<?>) value));
+        else if (value instanceof Map)
+            builder.append(new JsonObject((Map<?, ?>) value));
+        else
+            builder.append(value);
+        if (addComma)
+            builder.append(",");
+        builder.append("\n");
+    }
+
+    static Object warpValue(Object value) {
+        if(value == null) return null;
+        if (value instanceof Collection)
+            return new JsonArray((Collection<?>) value);
+        else if (value instanceof Map)
+            return new JsonObject((Map<?, ?>) value);
+        else
+            return value;
+    }
+
+    static char[] createTab(int tabSize, int index, char[] lastTab) {
+        char[] tab;
+        if (lastTab == null) {
+            tab = new char[tabSize];
+        } else {
+            tab = new char[index * tabSize];
+            System.arraycopy(lastTab, 0, tab, 0, lastTab.length);
+        }
+        for (int i = lastTab == null ? 0 : lastTab.length; i < tab.length; i++)
+            tab[i] = ' ';
+        return tab;
+    }
+
+    public static void makeQuote(String string, StringBuilder builder) {
+        if (string.isEmpty()) {
+            builder.append("\"\"");
+            return;
+        }
+
+        char lastChar;
+        char thisChar = 0;
+        String unicode;
+        int len = string.length();
+
+        builder.append('"');
+        for (int i = 0; i < len; i++) {
+            lastChar = thisChar;
+            thisChar = string.charAt(i);
+            switch (thisChar) {
+                case '\\':
+                case '"':
+                    builder.append('\\');
+                    builder.append(thisChar);
+                    break;
+                case '/':
+                    if (lastChar == '<')
+                        builder.append('\\');
+                    builder.append(thisChar);
+                    break;
+                case '\b':
+                    builder.append("\\b");
+                    break;
+                case '\t':
+                    builder.append("\\t");
+                    break;
+                case '\n':
+                    builder.append("\\n");
+                    break;
+                case '\f':
+                    builder.append("\\f");
+                    break;
+                case '\r':
+                    builder.append("\\r");
+                    break;
+                default:
+                    if (thisChar < ' ' || (thisChar >= '\u0080' && thisChar < '\u00a0') || (thisChar >= '\u2000' && thisChar < '\u2100')) {
+                        builder.append("\\u");
+                        unicode = Integer.toHexString(thisChar);
+                        builder.append("0000", 0, 4 - unicode.length());
+                        builder.append(unicode);
+                    } else
+                        builder.append(thisChar);
+            }
+        }
+        builder.append('"');
+    }
+
+    @Override
+    public Iterator<Map.Entry<String, Object>> iterator() {
+        return map.entrySet().iterator();
+    }
+
+    public HashMap<String, Object> getMap() {
+        return map;
     }
 }
